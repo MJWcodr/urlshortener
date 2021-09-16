4edda0b28667376fab8978d90b38a15bfb817ba3
MJWcodr <matthias.wuensch175@gmail.com>
2021-09-16 23:03:55 +0200

    add frontend as submodule

--- index.js
+++ index.js
@@ -6,11 +6,15 @@ const activeSubdomains = ['link', 'shorturls','/']
 // Dependencies
 const express = require('express');
 const app = express();
-const path = require('path');app.use(express.static(path.join(__dirname, 'build')));
+const path = require('path');app.use(express.static(path.join(__dirname, 'frontend/build')));
 const sqlite3 = require('sqlite3').verbose();
 const fs = require('fs')
 const favicon = require('express-favicon')
 const cors = require('cors')
+
+app.use(cors({
+    origin: 'http://ip-api.com/json'
+}));
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
@@ -37,19 +41,30 @@ app.use(favicon(path.join(__dirname, 'favicon.png')));
     });
 
     // Create DB if not already existent
-    const sql_create = `CREATE TABLE IF NOT EXISTS URL_table (
+    const create = [`CREATE TABLE IF NOT EXISTS URL_table (
         URL_ID INTEGER PRIMARY KEY AUTOINCREMENT,
         LongURL VARCHAR NOT NULL,
-        ShortURL VARCHAR NOT NULL UNIQUE
-      );`;
-      
-      db.run(sql_create, err => {
-        if (err) {
-          return console.error(err.message);
-        }
-        console.log("Successful creation of the 'URL' table");
+        ShortURL VARCHAR NOT NULL UNIQUE;
+      `, `CREATE TABLE IF NOT EXISTS Access (
+        ID         INTEGER PRIMARY KEY AUTOINCREMENT,
+        IP,
+        ShortURL           REFERENCES URL_table (ShortURL) 
+                           NOT NULL,
+        AccessTime TIME
+    );`] 
+        for (i=0; i > create.length; i++){
+            
+            
+            db.run(create[i], err => {
+               if (err) {
+                            return console.error(err.message);
+                        }
+            console.log(create[i])
+            console.log("Successful creation of the 'URL' table");
       });
-
+        }
+      
+      
 
 // Shorturl Routing and general responses
 app.get('/:id', (req,res) => {
@@ -62,15 +77,13 @@ app.get('/:id', (req,res) => {
         }
         if(row == null){
             if(activeSubdomains.includes(id)){
-                res.sendFile(path.join(__dirname, 'build/index.html'));
+                res.sendFile(path.join(__dirname, 'frontend/build/index.html'));
                 return
             }
             else{
                 app.use(express.static(path.join(__dirname, 'errors')));
                 res.sendFile(path.join(__dirname, 'errors/404.html'));
-                
             }
-            
         }
         else {
             res.redirect(row.LongURL)
@@ -78,14 +91,14 @@ app.get('/:id', (req,res) => {
     })
 });
 
-      
+
 // Post /created
 app.post ('/created', (req, res, next) => {
     var sql = "INSERT INTO URL_table (LongURL, ShortURL) VALUES (?,?)"
     const URL = [req.body.LongURL, req.body.ShortURL]
     db.run(sql, URL, err => {
         res.sendStatus(200)
-        console.log(req.body)
+        console.log("new entry: " + req.body)
     })
 })
 
