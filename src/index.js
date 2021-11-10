65f12903f4844927c4d90907a1dbe2ae37576037
Matthias Wünsch <matthias.wuensch175@gmail.com>
2021-11-10 10:22:11 +0100

    remove unnecessary files

--- src/index.js
+++ src/index.js
@@ -3,10 +3,9 @@ import ReactDOM from 'react-dom';
 import { Route, BrowserRouter as Router } from 'react-router-dom'
 
 // React components
-    import LongApp from './components/LongShort/Long-App'; 
-    import linktree from './components/linktree/linktree';
+import LongApp from './components/LongShort/Long-App';
+import linktree from './components/linktree/linktree';
 // Resources
-import reportWebVitals from './reportWebVitals';
 
 // Routing
 const routes = (
@@ -19,8 +18,3 @@ const routes = (
 ReactDOM.render(routes,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
